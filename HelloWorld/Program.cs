0c9decff9b52fc1709277dc6ef8048b8ae7ae90d
Matthew Luedtke <MLuedtke@tql.com>
2021-05-18 16:29:09 -0400

    created classes, methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,31 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World in C#");
+            var sarah = new Student();
+            sarah.Firstname = "Sarah";
+            sarah.Lastname = "George";
+            sarah.SetHireDate(2017, 6, 26);
+
+            var charlie = new Student();
+            charlie.Firstname = "Charlie";
+            charlie.Lastname = "Traylor";
+
+            var mattea = new Student();
+            mattea.Firstname = "Mattea";
+            mattea.Lastname = "Swain";
+
+
+            sarah.Print();
+            charlie.Print();
+            mattea.Print();
+
+
+            //int years = 7;
+            //var company = "TQL";
+            //var student = "Matt";
+            //Console.WriteLine($"hello {student} from {company}");
+            //Console.WriteLine($"my name is {student} and i've been at {company} for {years} years");
+
         }
     }
 }
---
+++ HelloWorld/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Student
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public bool Active { get; set; }
+        public DateTime HireDate { get; set; }
+
+
+        public void SetHireDate(int year, int month, int day)
+        {
+            if(year <1950 || year > 2200)
+            {
+                Console.WriteLine("year must be between 1950 and 2200");
+                return;
+            }
+            if(month <1 || month > 12)
+            {
+                Console.WriteLine("need valid month");
+                return;
+            }
+            if (day < 1 || day > 31)
+            {
+                Console.WriteLine("need valid day");
+                return;
+            }
+            HireDate = new DateTime(year, month, day);
+        }
+
+        public void Print() {
+            Console.WriteLine($"{Firstname} {Lastname} {HireDate.ToString("MM/dd/yyyy")}");
+
+        }
+
+    }
+}
